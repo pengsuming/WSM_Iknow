00536f5619f59c7512ce13bab76f3e556320248f
pengsuming <w2212@e-mirim.hs.kr>
2024-06-28 14:08:16 +0900

    UIUX과제를 하면서 좀더 보완을 하게된 나의 코드 - 지도API 사용해서 위치 알려줌

---
+++ js/headerbar.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', checkViewportSize);
+            window.addEventListener('resize', checkViewportSize);
+
+            const toggleBtn = document.querySelector('#navbar__toggleBtn');
+            const menu = document.querySelector('.navbar__menu');
+            const icons = document.querySelector('.navbar__icons');
+    
+            toggleBtn.addEventListener('click', () => {
+                menu.classList.toggle('active');
+                icons.classList.toggle('active');
+            });
